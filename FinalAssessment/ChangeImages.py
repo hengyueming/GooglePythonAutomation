531ea0129f059b8a7c4a8975b877b71ae8fe9dc2
hengyueming <51424722+hengyueming@users.noreply.github.com>
2021-03-19 11:01:14 -0400

    Update ChangeImages.py
    change tiff type Images in the directory file

--- FinalAssessment/ChangeImages.py
+++ FinalAssessment/ChangeImages.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+import os
+import sys
+from PIL import Image
 
+path = 'supplier-data/images/'
+image_dir = os.listdir(path)
+
+for image in image_dir:
+        if 'tiff' in image:
+                file_name = os.path.splitext(image)[0]
+                im = Image.open(path + image)
+                im.convert('RGB').resize((600,400)).save(path + file_name + '.jpeg', 'JPEG')
