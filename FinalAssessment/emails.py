87ae31fb25ec035a6ea06aa389637ed4c156a2a3
hengyueming <51424722+hengyueming@users.noreply.github.com>
2021-03-20 09:59:50 -0400

    Update emails.py
    fix errors

--- FinalAssessment/emails.py
+++ FinalAssessment/emails.py
@@ -5,23 +5,24 @@ import mimetypes
 import os.path
 import smtplib
 
-def generate_email(sender, recipient, subject, body, attachment_path):
+def generate_email(sender, recipient, subject, body, attachment_path = None):
   message = email.message.EmailMessage()
   message['From'] = sender
   message['To'] = recipient
   message['Subject'] = subject
   message.set_content(body)
   
-  attachment_filename = os.path.basename(attachment_path)
-  mime_type, _ = mimeytpes.guess_type(attachment_path)
-  mime_type, mime_subtype = mime_type.split('/', 1)
+  if attachment_path != None:
+    attachment_filename = os.path.basename(attachment_path)
+    mime_type, _ = mimeytpes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/', 1)
   
-  with open(attachment_path, 'rb') as ap:
-    message.add_attachement(ap.read(),
-                            maintype=mime_type,
-                            subtype=mime_subtype,
-                            filename=attachment_filename)
-  return message
+    with open(attachment_path, 'rb') as ap:
+      message.add_attachment(ap.read(),
+                              maintype=mime_type,
+                              subtype=mime_subtype,
+                              filename=attachment_filename)
+    return message
 
 def send(message):
   mail_server = smtplib.SMTP('localhost')
