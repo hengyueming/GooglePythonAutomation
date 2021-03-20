80fe67ee98fad25e3d9fb699b59dba72c0ad726a
hengyueming <51424722+hengyueming@users.noreply.github.com>
2021-03-20 09:08:40 -0400

    Update report_emails.py

--- FinalAssessment/report_emails.py
+++ FinalAssessment/report_emails.py
@@ -2,14 +2,34 @@
 import emails
 import reports
 import os
-imoirt sys
+import sys
+import datetime
 
-report_path = '/home/username'
-sender = "automation@example.com"
-receiver = "{}@example.com".format(os.environ.get('USER'))
-subject = 'Upload Completed - Online Fruit Stroe'
-body = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email'
-table_data = 
-reports.generate_report(report_path, 'Processed Update on ' + os.getdate(), table_data)
-message = emails.generate_email(sender, receiver, subject, body, report_path)
-email.send(message)
+path = 'supplier-data/descriptions/'
+files = os.listdir(path)
+date = datetime.datetime.now().strftime('%m-%d-%Y')
+
+def get_data(files):
+  data = ""
+  for file in files:
+    if file.endswith('.txt'):
+      with open(path + file, 'r') as f:
+        line = f.readline()
+        name = line[0].strip()
+        weight = line[1].strip()
+        data += 'name: ' + name + '</br>' + 'weight: ' + weight = '</br></br>'
+  return data
+
+if __name__ == "__main__":
+  table_data = get_data(files)
+  report_path = '/tmp/porcessed.pdf'
+  reports.generate_report(report_path, 'Processed Update on ' + date, table_data)
+  
+  sender = "automation@example.com"
+  receiver = "{}@example.com".format(os.environ.get('USER'))
+  subject = 'Upload Completed - Online Fruit Stroe'
+  body = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email.'
+  attachment = '/tmp/processed.pdf'
+  message = emails.generate_email(sender, receiver, subject, body, report_path)
+  
+  email.send(message)
