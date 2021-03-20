0b66d49d40b41e89aa3b0d2d45318b1c54315bea
hengyueming <51424722+hengyueming@users.noreply.github.com>
2021-03-20 08:28:01 -0400

    Update report_emails.py

--- FinalAssessment/report_emails.py
+++ FinalAssessment/report_emails.py
@@ -4,8 +4,12 @@ import reports
 import os
 imoirt sys
 
+report_path = '/home/username'
 sender = "automation@example.com"
 receiver = "{}@example.com".format(os.environ.get('USER'))
 subject = 'Upload Completed - Online Fruit Stroe'
 body = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email'
-message = emails.generate_email(sender, receiver, 
+table_data = 
+reports.generate_report(report_path, 'Processed Update on ' + os.getdate(), table_data)
+message = emails.generate_email(sender, receiver, subject, body, report_path)
+email.send(message)
