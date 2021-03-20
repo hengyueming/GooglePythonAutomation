f78349fa2b23b9fafd6006de4001544dcc8d50e0
hengyueming <51424722+hengyueming@users.noreply.github.com>
2021-03-20 10:01:32 -0400

    Update emails.py

--- FinalAssessment/emails.py
+++ FinalAssessment/emails.py
@@ -12,17 +12,16 @@ def generate_email(sender, recipient, subject, body, attachment_path = None):
   message['Subject'] = subject
   message.set_content(body)
   
-  if attachment_path != None:
-    attachment_filename = os.path.basename(attachment_path)
-    mime_type, _ = mimeytpes.guess_type(attachment_path)
-    mime_type, mime_subtype = mime_type.split('/', 1)
+   attachment_filename = os.path.basename(attachment_path)
+   mime_type, _ = mimeytpes.guess_type(attachment_path)
+   mime_type, mime_subtype = mime_type.split('/', 1)
   
-    with open(attachment_path, 'rb') as ap:
-      message.add_attachment(ap.read(),
-                              maintype=mime_type,
-                              subtype=mime_subtype,
-                              filename=attachment_filename)
-    return message
+   with open(attachment_path, 'rb') as ap:
+     message.add_attachment(ap.read(),
+                             maintype=mime_type,
+                             subtype=mime_subtype,
+                             filename=attachment_filename)
+   return message
 
 def send(message):
   mail_server = smtplib.SMTP('localhost')
